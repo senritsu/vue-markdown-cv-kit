83d90c4b4dc588d6ed1c3980ef272bbaefcba542
senritsu <jonas.schiegl@gmail.com>
2023-12-11 16:12:37 +0100

    support markdown for the summary

--- lib/ProfileSummary.vue
+++ lib/ProfileSummary.vue
@@ -1,16 +1,24 @@
 <script setup lang="ts">
+import VueMarkdown from 'vue-markdown-render'
+
 defineProps<{
   summary?: string
 }>()
 </script>
 
 <template>
-  <p>{{ summary }}</p>
+  <VueMarkdown class="summary" :source="summary" />
 </template>
 
 <style scoped>
-p {
+.summary {
   color: var(--color-text);
   font-size: 9pt;
+}
+
+.summary :deep(a),
+.summary :deep(a:visited) {
+  color: var(--color-awesome);
+  text-decoration: none;
 }
 </style>
